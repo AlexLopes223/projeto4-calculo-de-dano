605a5b368d80fdc7501132579eb51f47f370a485
Alex Lopes da Silva <alex984490566@gmail.com>
2024-09-18 19:37:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const pers1 = prompt('Defina o nome do seu personagem')
 const pers2 = prompt('Defina o nome de outro personagem')
-let attackPower = parseInt(prompt('Quantos vale o poder de ataque de ${pers1}?'))
-let life = parseInt(prompt('Quantos pontos de vida  ${pers2] tem?'))
-let defensePower = paseInt(prompt(Quantos pontos de defesa ${pers2}Tem?''))
-let haveShilde = prompt('$pers2} tem um escudo?')
+let attackPower = parseInt(prompt(`Quantos vale o poder de ataque de ${pers1}?`))
+let life = parseInt(prompt(`Quantos pontos de vida  ${pers2] tem?`))
+let defensePower = paseInt(prompt(`Quantos pontos de defesa ${pers2}Tem?`))
+let haveShilde = prompt(`$pers2} tem um escudo?`)
 let difference = 0
 let damage = 0
 let damageInlife= 0
@@ -13,10 +13,10 @@ if (attacckPower > defensePower $ haveShilde === 'Não' || haveSheld === 'não')
    demage = difference
    demageInlife = damage - life
    if(damageInlife < 0) {damageInlife *= -1
-    alert('${pers1}' atacou ${'pers2'} sofreu ${damage} de dano e não possui escudo. Sua vida agora é ${damageInlife})
+    alert(`${pers1} atacou ${pers2} sofreu ${damage} de dano e não possui escudo. Sua vida agora é ${damageInlife}`)
 }}
 else {
-   alert('${pers1} atacou ${pers2} sofreu ${damge} de dano e não possui escudo')}
+   alert(`${pers1} atacou ${pers2} sofreu ${damge} de dano e não possui escudo`)}
 
    else if (attackPower > defensePower && haveShield === 'Sim' || haveShield === 'sim') {
     difference = (attackPower - defensePower) / 2
@@ -30,4 +30,3 @@ else {
     }
 } else if (attackPower <= defensePower) {
     alert(`${pers1} atacou, mas não afetou ${pers2}. Nenhum dano causado!`)
-}
